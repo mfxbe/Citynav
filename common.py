af17cb6e9473f1be052333b8cd19eb5fad937108
Manuel Ehrmanntraut <mfxbe@feritale.eu>
2024-08-27 19:49:22 +0200

    Fix color from express buses + code style

--- common.py
+++ common.py
@@ -57,6 +57,7 @@ class MyPage(ft.AnimatedSwitcher):
 def color_allocator(line):
 	color = None
 	fixed = {"U1": "#3c7333", "U2": "#c3022d", "U3": "#ed6720", "U4": "#00ab85", "U5": "#bd7b00", "U6": "#0065b0",
+			 "U7": "#c0ba79", "U8": "#c53f09",
 			 "S1": "#0ec1ea", "S2": "#72c042", "S3": "#7c087e", "S4": "#ef1620", "S5": "#ffcd00", "S6": "#008b50",
 			 "S7": "#973530", "S8": "#000000", "S20": "#f15a74"}
 
@@ -73,17 +74,20 @@ def color_allocator(line):
 			color = "#d91a1a"
 		else:
 			color = "#00576a"
+	elif line.startswith("X"):
+		color = "#00576a"
 	else:
 		color = "#aeaeaf"
 
 	return color
 
+
 class StorageHandler():
 	def __init__(self, page):
 		self.p = page
-		self.prefix="de.mfxbe.Citynav."
+		self.prefix = "de.mfxbe.Citynav."
 
-		#Set defaults
+		# Set defaults
 		self.theme = self.set_from_storage("theme", "Auto")
 		self.results = self.set_from_storage("results", 1)
 		self.connection_history = self.set_from_storage("connection_history", list())
@@ -102,13 +106,14 @@ class StorageHandler():
 
 	def get_key(self, key):
 		value = self.p.client_storage.get(self.prefix + key)
-		return(value)
+		return value
 
 	def set_from_storage(self, key, default):
 		result = default
 
 		try:
-			if self.p.client_storage.contains_key(self.prefix + key) or self.p.client_storage.get(self.prefix + key) != None:
+			if self.p.client_storage.contains_key(self.prefix + key) or self.p.client_storage.get(
+					self.prefix + key) is not None:
 				result = self.p.client_storage.get(self.prefix + key)
 		except Exception as e:
 			print(e)
@@ -116,4 +121,4 @@ class StorageHandler():
 			self.p.snack_bar.open = True
 			self.p.update()
 
-		return(result)
+		return result
