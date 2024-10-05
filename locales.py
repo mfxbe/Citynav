62a41c012f816ce6c57ba677a9b12d9b9d48590b
Manuel Ehrmanntraut <dev@mfxbe.de>
2024-10-05 21:38:22 +0200

    Try own implementation

--- locales.py
+++ locales.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 import locale
 import importlib
+from urllib.parse import urlparse, parse_qs
 
-try:
-	l = locale.getlocale()[0][:2]
-	myl10n = importlib.import_module('locales_data.' + l)
-except:
-	myl10n = lambda: None
-	myl10n.data = dict()
+myl10n = lambda: None
+myl10n.data = dict()
 
 # support multiple languages (would prefer gettext for that by I do not manage to get it working in web & android builds)
 # this small modul is written in a way easily replaceable by gettext
@@ -19,3 +16,22 @@ def _(string):
 		return myl10n.data[string]
 	else:
 		return string
+
+def set_up_locales(page):
+	global myl10n
+	l = None
+
+	#try:
+	#	l = parse_qs(urlparse(page.url).query)["lang"][0]
+	#except:
+	#	l = None
+
+	try:
+		if l is None:
+			l = locale.getlocale()[0][:2]
+		if l is None:
+			l = "de"
+		myl10n = importlib.import_module('locales_data.' + l)
+	except Exception as e:
+		print("s " + str(e))
+		myl10n.data = dict()
